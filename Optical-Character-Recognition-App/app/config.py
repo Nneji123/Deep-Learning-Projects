71d2cf573863250665902928b88b9a40e8a3a5c8
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-23 21:04:11 +0000

    made updates

---
+++ Optical-Character-Recognition-App/app/config.py
@@ -0,0 +1,23 @@
+import os
+from os import environ
+
+class Config(object):
+
+    DEBUG = False
+    TESTING = False
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    SECRET_KEY = 'pianalytix'
+
+    UPLOADS = "/home/username/app/app/static/uploads"
+
+    SESSION_COOKIE_SECURE = True
+    DEFAULT_THEME = None
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SESSION_COOKIE_SECURE = False
+
+class DebugConfig(Config):
+    DEBUG = False
