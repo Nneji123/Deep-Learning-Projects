bbc4d8b731a4fc1412b05230602beeba3d8f3669
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-24 18:02:39 +0000

    created donkeys dataset

--- Zebra-Donkey-Horse-Classifier-Pytorch/downloader.py
+++ Zebra-Donkey-Horse-Classifier-Pytorch/downloader.py
@@ -1,3 +1,3 @@
 from bing_image_downloader import downloader
-downloader.download('horses', limit=2000,  output_dir='', page_limit=1000, adult_filter_off=True, force_replace=False, timeout=20, resize=(224,224) ,verbose=True)
+downloader.download('donkeys', limit=2000,  output_dir='', page_limit=3000, adult_filter_off=True, force_replace=False, timeout=20, resize=(224,224) ,verbose=True)
 
