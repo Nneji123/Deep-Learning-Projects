324ec7d671c30f8a63caaf4225d520d478a535a0
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-07-24 07:57:44 +0000

    updates

---
+++ Nike-Adidas-Shoes-Classification-CNN/train.py
@@ -0,0 +1,34 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout 
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+
+from sklearn.metrics import classification_report,confusion_matrix
+
+import tensorflow as tf
+
+import cv2
+import os
+
+import numpy as np
+
+labels = ['rugby', 'soccer']
+img_size = 224
+def get_data(data_dir):
+    data = [] 
+    for label in labels: 
+        path = os.path.join(data_dir, label)
+        class_num = labels.index(label)
+        for img in os.listdir(path):
+            try:
+                img_arr = cv2.imread(os.path.join(path, img))[...,::-1] #convert BGR to RGB format
+                resized_arr = cv2.resize(img_arr, (img_size, img_size)) # Reshaping images to preferred size
+                data.append([resized_arr, class_num])
+            except Exception as e:
+                print(e)
+    return np.array(data)
+
+Now we can easily fetch our train and validation data.
+train = get_data('../input/traintestsports/Main/train')
+val = get_data('../input/traintestsports/Main/test')
